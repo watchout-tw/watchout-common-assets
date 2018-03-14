61084d18ac3c01ea3c6c5a1e6641db600b359c53
chihao <chihaoyo@gmail.com>
2018-03-14 17:25:46 +0800

    Move announcements to env

--- environment/index.js
+++ environment/index.js
@@ -16,8 +16,27 @@ const links = {
   },
   contact: rootURL + 'contact'
 }
+const announcements = {
+  rows: [
+    {
+      type: 'breaking',
+      channel: 'system',
+      content: '今日下午5:87在火星東經123度、北緯23.5度發生規模4.2級的地震。'
+    },
+    {
+      type: 'livestream',
+      channel: 'ask',
+      content: '2018台北市長給問嗎？第一場辯論會直播即將開始！',
+      action: {
+        label: '點我前往',
+        link: 'https://waa.tw/g90Jdf'
+      }
+    }
+  ]
+}
 
 export default {
   channels,
-  links
+  links,
+  announcements
 }
